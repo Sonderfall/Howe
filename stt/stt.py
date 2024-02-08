9b12429883c7377364ce53059df1c9ae31be4d34
vmacheret <vmacheret@ext.scaleway.com>
2024-02-08 22:32:06 +0100

    feat(ai): refacto for stt-llama-tts ai

---
+++ stt/stt.py
@@ -0,0 +1,12 @@
+from google.cloud import speech
+
+def speech_to_text(
+    config: speech.RecognitionConfig,
+    audio: speech.RecognitionAudio,
+) -> speech.RecognizeResponse:
+    client = speech.SpeechClient()
+
+    # Synchronous speech recognition request
+    response = client.recognize(config=config, audio=audio)
+
+    return response
