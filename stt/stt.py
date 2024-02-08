1120b5eb50da20085cd90d778fd79d516153c5c8
vmacheret <vmacheret@ext.scaleway.com>
2024-02-08 23:51:02 +0100

    feat(ai): refacto for stt-llama-tts ai

--- stt/stt.py
+++ stt/stt.py
@@ -1,12 +1,28 @@
-from google.cloud import speech
+from google.cloud import speech_v2
+from google.oauth2 import service_account
 
-def speech_to_text(
-    config: speech.RecognitionConfig,
-    audio: speech.RecognitionAudio,
-) -> speech.RecognizeResponse:
-    client = speech.SpeechClient()
+
+## AIzaSyDGZAZgLoynElueG1fWaBxSOpp94xt4d58
+
+def listen(callback):
+    credentials = service_account.Credentials.from_service_account_file('config/gcloud.json')
+    # credentials = service_account.Credentials
+    client = speech_v2.SpeechClient(credentials=credentials)
+
+    # Initialize request argument(s)
+    request = speech_v2.RecognizeRequest(
+        content=b'content_blob',
+        recognizer="recognizer_value",
+    )
 
     # Synchronous speech recognition request
-    response = client.recognize(config=config, audio=audio)
+    response = client.recognize(request=request)
+
+    for _, r in enumerate(response.results):
+        for _, a in enumerate(r.alternatives):
+            print(a.words)
 
     return response
+
+if __name__ == "__main__":
+    listen(None)
--- tts/tts.py
+++ tts/tts.py
@@ -5,32 +5,39 @@ Note: ssml must be well-formed according to:
     https://www.w3.org/TR/speech-synthesis/
 """
 from google.cloud import texttospeech
+from google.oauth2 import service_account
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+def say(utterance: str):
+    credentials = service_account.Credentials.from_service_account_file('config/gcloud.json')
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient(credentials=credentials)
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-)
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=utterance)
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="fr-CA", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+    )
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
 
-# The response's audio_content is binary.
-with open("output.mp3", "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open("output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+if __name__ == "__main__":
+    say("Bonjour, je m'appelle Valentin")
